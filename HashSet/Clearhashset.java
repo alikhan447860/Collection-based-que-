7507caa4f728199b31b9e5798310d9707edc6d71
Ali khan <khanali6861@gmail.com>
2024-11-03 14:04:50 +0530

    HashSet

---
+++ HashSet/Clearhashset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Clearhashset{
+public static void main(String[] args) {
+    HashSet<Integer>hs=new HashSet<>();
+    hs.add(1);
+    hs.add(4);
+    hs.add(99);
+    hs.clear();
+    System.out.println(hs);
+}
+}
